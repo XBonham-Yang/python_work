7eaab34aa7b0904196c4ecd50c118610ed3070fa
XBonham-Yang <107122564+XBonham-Yang@users.noreply.github.com>
2022-08-04 13:45:00 +0100

    snake game part one

---
+++ snake_game/snake.py
@@ -0,0 +1,44 @@
+from turtle import Turtle
+MOVESPEED = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+class Snake:
+    def __init__(self):
+        self.snake = []
+        self.create_snake()
+        self.head = self.snake[0]
+
+    def create_snake(self):
+        for i in range(3):
+            tim = Turtle()
+            tim.shape('square')
+            tim.color('white')
+            tim.penup()
+            tim.goto(-20 + 20 * i, 0)
+            self.snake.append(tim)
+
+
+    def move(self):
+        for i in range(len(self.snake) - 1, 0, -1):
+            new_x = self.snake[i - 1].xcor()
+            new_y = self.snake[i - 1].ycor()
+            self.snake[i].goto(new_x, new_y)
+            self.snake[0].forward(MOVESPEED)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
