f025e132bec28d1a6847f3964e48dd550d79dda8
XBonham-Yang <107122564+XBonham-Yang@users.noreply.github.com>
2022-08-10 19:11:06 +0100

    finished

--- Pong/ball.py
+++ Pong/ball.py
@@ -9,11 +9,21 @@ class Ball(Turtle):
         self.goto((0,0))
         self.x_move = 10
         self.y_move = 10
+        self.movespeed = 0.1
 
     def up(self):
         self.goto((self.xcor()+self.x_move,self.ycor()+self.y_move))
 
 
-    def bounce(self):
+    def bounce_y(self):
         self.y_move *= -1
 
+    def bounce_x(self):
+        self.x_move *= -1
+        self.movespeed * 0.9
+
+
+    def reset(self):
+        self.goto(0,0)
+        self.bounce_x()
+        self.movespeed = 0.1
--- Pong/main.py
+++ Pong/main.py
@@ -2,6 +2,7 @@ from turtle import Screen
 from Paddle import Paddle
 from ball import Ball
 import time
+from scoreboard import Scoreboard
 # screen
 screen = Screen()
 screen.bgcolor("black")
@@ -12,6 +13,7 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -22,12 +24,34 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_on = True
 while game_on:
-    time.sleep(0.1)
+    time.sleep(ball.movespeed)
     screen.update()
     ball.up()
 
     if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce()
+        ball.bounce_y()
+
+# detect paddles
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320:
+            ball.bounce_x()
+            scoreboard.r_point()
+            scoreboard.update()
+
+    if ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+        scoreboard.l_point()
+        scoreboard.update()
+
+# missing the ball
+    if ball.xcor() > 380:
+        ball.reset()
+        scoreboard.l_point()
+        scoreboard.update()
+
+    if ball.xcor() < -380:
+        ball.reset()
+        scoreboard.r_point()
+        scoreboard.update()
 
 
 
